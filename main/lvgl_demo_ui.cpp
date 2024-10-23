721ac8f1fa4ff4b21ad673b5fe3fe117b65f8e5d
cadantas33 <caiocd7@protonmail.com>
2024-10-23 19:13:44 -0300

    correções gerais

--- main/lvgl_demo_ui.cpp
+++ main/lvgl_demo_ui.cpp
@@ -5,14 +5,21 @@
  */
 
 #include "lvgl.h"
+#include "cBMP280.h"
+
+cBMP280 BMP280;
 
 void example_lvgl_demo_ui(lv_disp_t *disp)
 {
-    lv_obj_t *scr = lv_disp_get_scr_act(disp);
-    lv_obj_t *label = lv_label_create(scr);
-    lv_label_set_long_mode(label, LV_LABEL_LONG_SCROLL_CIRCULAR); /* Circular scroll */
-    lv_label_set_text(label, "Hello Espressif, Hello LVGL.");
-    /* Size of the screen (if you use rotation 90 or 270, please set disp->driver->ver_res) */
-    lv_obj_set_width(label, 128);
-    lv_obj_align(label, LV_ALIGN_TOP_MID, 0, 0);
+    
+    float temp = BMP280.getTemperature();
+    
+    lv_obj_t *scr = lv_disp_get_scr_act(nullptr);
+    lv_obj_t *labelBMP280Temp = lv_label_create(scr);
+    lv_label_set_long_mode(labelBMP280Temp, LV_LABEL_LONG_SCROLL_CIRCULAR);
+    //lv_label_set_text_fmt(labelBMP280Temp, "%6.0f", temp);
+    lv_label_set_text(labelBMP280Temp, "Hello");
+    lv_obj_set_width(labelBMP280Temp, 128);
+    lv_obj_align(labelBMP280Temp, LV_ALIGN_TOP_MID, 0, 16);
+
 }
